91527ca1a95acdf8ab05095ef9139b74d449bd61
WinterSunset95 <wintersunset95@gmail.com>
2024-04-20 16:47:51 +0530

    More socket.io trial

--- src/lib/socketio.ts
+++ src/lib/socketio.ts
@@ -1,7 +1,21 @@
 import ioClient from 'socket.io-client'
-//const ENDPOINT = 'http://localhost:3000'
-const ENDPOINT = 'https://dear-code-backend.onrender.com'
 
-const socket = ioClient(ENDPOINT)
+// Check if localhost or render.com
+async function checkHost() {
+	const host = window.location.host
+	if (host.includes('localhost')) {
+		return 'http://localhost:3000'
+	} else {
+		return 'https://dear-code-backend.onrender.com'
+	}
+}
+
+const ENDPOINT = await checkHost()
+//const ENDPOINT = 'http://localhost:3000'
+//const ENDPOINT = 'https://dear-code-backend.onrender.com'
+
+const socket = ioClient(ENDPOINT, {
+	transports: ['websocket', 'polling', 'flashsocket']
+})
 
 export const io = socket;
